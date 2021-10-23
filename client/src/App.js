5427edf56ce05a06f03ef018d06761c562a6ee74
j <jsn.chn@outlook.com>
2021-10-23 12:56:26 -0700

    Init react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <p>hello world</p>
+    <Router>
+      <p>I have...</p>
+      <Link to="/questions">questions</Link>
+      <Link to="/answers">answers</Link>
+      <Switch>
+      <Route path="/questions">
+        <p>hello world questions</p>
+      </Route>
+      <Route path="/answers">
+        <p>hello world answers</p>
+      </Route>
+      </Switch>
+    </Router>
   );
 }
 
