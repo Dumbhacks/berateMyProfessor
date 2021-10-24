dc6fd72f5130ca565e7a1aa384832500ecdc0185
j <jsn.chn@outlook.com>
2021-10-23 19:53:12 -0700

    Pause on a bug

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -1,20 +1,35 @@
 'use strict';
+// import rooms model
+const Room = require("../models/roomsModel");
+const bodyParser = require('body-parser');
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // create App function
 module.exports = function(app) {
     var rooms = require('../controllers/controllers.js');
 
-    // app
-    // .route("/rooms")
-    // .get(rooms.listAllRooms)
-    // .post(rooms.createRoom);
-
-    app.get("/rooms", (req, res) => {
-      console.log("rooms get")
-      return rooms.listAllRooms;
-    })
-
-    app.post("/rooms", (req, res) => {
-      return rooms.createNewRoom;
-    })
+    app.get("/rooms", urlencodedParser, (req, res) => {
+      Room.find({}, (err, room) => {
+        console.log("list all rooms")
+        if (err) {
+        res.status(500).send(err);
+        }
+        res.status(200).json(room);
+        });
+      })
+    .post("/rooms", urlencodedParser, (req, res) => {
+      console.log(req.body);
+      let newRoom = new Room(req.body);
+      console.log("create new room");
+      newRoom.save((err, room) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.status(201).send(req.body);
+      });
+    });
 }
